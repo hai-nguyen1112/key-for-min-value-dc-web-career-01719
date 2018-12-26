f5c2ca6c9caf85faec72fbb687bfa54d3ddee1e5
hai-nguyen1112 <hainguyen871112@gmail.com>
2018-12-26 17:22:10 -0500

    My key for min value commit

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil
+  end
+  value_min = 0
+  key_min = nil
+  name_hash.collect do |name, value|
+    if value_min == 0
+      value_min = value
+      key_min = name
+    elsif value < value_min
+      value_min = value
+      key_min = name
+    end
+  end
+  return key_min
 end
